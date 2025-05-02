ffb92eb924eb437c1cc64f1781a6214d45547f5e
bit92 <sahabithun92@gmail.com>
2025-05-02 12:08:44 +0200

    initial commit

---
+++ src/main/java/se/bithun/javawebdemo/controllers/HomeController.java
@@ -0,0 +1,29 @@
+package se.bithun.javawebdemo.controllers;
+
+import org.bithun.javawebdemo.models.POPSingerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class HomeController extends BaseController {
+    @Autowired
+    POPSingerRepository popsingerRepository;
+    @GetMapping("/")
+    public String home(Model model) {
+
+        model.addAttribute("singer", popsingerRepository.findAll());
+        model.addAttribute("user", "Mim");
+
+        return "home";
+    }
+    @GetMapping("/profile")
+    public String profile(Model model) {
+        String user = getLoggedInEmail();
+        model.addAttribute("user", user);
+        return "profile";
+    }
+}
---
+++ src/main/java/se/bithun/javawebdemo/models/POPSingerRepository.java
@@ -0,0 +1,8 @@
+package se.bithun.javawebdemo.models;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.bithun.pos.javawebdemoapril.models.popSinger;
+
+public interface POPSingerRepository extends JpaRepository<popSinger, Integer> {
+
+}
